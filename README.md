3c0a6347e112129d7a7b258c0057b3703e40879d
Malik_Shanaah <s11717878@stu.najah.edu>
2022-04-08 00:56:13 +0300

    AI-Conversation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Alan-AI-Voice-Controlled-Magazine
 # In progress.....
+# Conversational voice controlled React.js news application using AlanAI.
--- alan/src/App.js
+++ alan/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
+const alanKey = '29ee32de4916668c580f5060db3698e92e956eca572e1d8b807a3e2338fdd0dc/stage'
 const App = ()=> {
-  return (
-    <div>
-        Hello, My name is Malik Shanaah
-    </div>
-  );
+    useEffect(()=>{
+      alanBtn({
+        key: alanKey,
+        onCommand:({command, articles})=>{
+          if(command === 'newsHeadlines'){
+            console.log(articles)
+          }
+        }
+      })
+    }, [])
 }
 
 export default App;
