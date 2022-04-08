b89cb29584dd5f9da93d20a52398abc8bc1e8896
Malik_Shanaah <s11717878@stu.najah.edu>
2022-04-08 15:34:34 +0300

    ....

--- alan/src/Components/NewsCards/NewsCards.js
+++ alan/src/Components/NewsCards/NewsCards.js
@@ -3,23 +3,23 @@ import NewsCard from '../NewsCard/NewsCard';
 import {Grid, Grow, Typography} from '@mui/material'
 import useStyles from './Styles.js'
 
-const NewsCards = ({articles}) => {
-    
-   const classes = useStyles()
 
+
+const NewsCards = ({articles}) => {
+    const classes = useStyles()
   return (
-     <Grow in>
-         <Grid className={classes.container} container alignItems='stretch' spacing={3}>
-              {
-                  Object.key(articles).map((article, i)=>(
-                      <Grid key={i} item xs={12} sm ={6} md ={4} lg ={3} style={{display:'flex'}}>
-                             <NewsCard article={article} i={i} />
-                      </Grid>
-                  ))
-              }
-         </Grid>
-     </Grow>
-    
+<Grow in>
+      <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+         {
+             Object.keys(articles).map((article, i)=>(
+                 <Grid key={i} item xs={12} sm ={6} md ={4} lg ={3} style={{display:'flex'}}>
+                        <NewsCard article={article} i={i} />
+                 </Grid>
+             ))
+         }
+      </Grid>
+</Grow>
+ 
   )
 }
 
