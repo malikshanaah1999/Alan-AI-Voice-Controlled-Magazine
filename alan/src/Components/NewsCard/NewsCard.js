f0caea9af6b457b525a4c9fdd381425b42d8513a
Malik_Shanaah <s11717878@stu.najah.edu>
2022-04-12 04:54:46 +0300

    ...

--- alan/src/Components/NewsCard/NewsCard.js
+++ alan/src/Components/NewsCard/NewsCard.js
@@ -1,31 +1,53 @@
 import React from 'react'
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@mui/material'
 
+
 const NewsCard = (props) => {
-   
+    const styleCard = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        borderBottom: '10px solid white',
+        
+    }
+    const styleDiv = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '20px',
+    }
+    const styleTypo = {
+        padding: '0 16px',
+    }
+    const styleAction = {
+        padding: '0 16px 8px 16px',
+        display: 'flex',
+        justifyContent: 'space-between',
+      }
+    
 
    const a = props.article.articles[props.count].description
    const b = props.article.articles[props.count].title
    const c = props.article.articles[props.count].source.name
    const d = props.article.articles[props.count].publishedAt
    const x = props.article.articles[props.count].urlToImage
-   
+   const z = props.article.articles[props.count].url
+   // Styles for the cardMedia must be declared to show the image, otherwise the image won't be  shown
 
   return (
-    <Card>
-        <CardActionArea>
-            <CardMedia image={x || 'Error'} />
-            <div>
+    <Card style={styleCard}>
+        <CardActionArea href={z} target ='_blank'>
+            <CardMedia image={x} style={{height:150,}} /> 
+            <div  style={styleDiv}>
                 <Typography variant='body2' color='textSecondary' component='h2' >{(new Date(d)).toDateString()}</Typography>
                 <Typography variant='body2' color='textSecondary' component='h2' >{c}</Typography>
             </div>
-            <Typography gutterBottom variant='h5' >{b}</Typography>
+            <Typography gutterBottom variant='h5' style={styleTypo} >{b}</Typography>
             <CardContent>
                  <Typography variant='body2' color='textSecondary' component='h2' >{a}</Typography>
             </CardContent>
         </CardActionArea>
-        <CardActions>
-            <Button size='small' color='primary' >Learn more</Button>
+        <CardActions style={styleAction}>
+            <Button href={z} size='small' color='primary' >Learn more</Button>
             <Typography variant='h5' color='textSecondary' >{1+(props.count)}</Typography>
         </CardActions>
     </Card>
--- alan/src/Components/NewsCards/NewsCards.js
+++ alan/src/Components/NewsCards/NewsCards.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import NewsCard from '../NewsCard/NewsCard';
 import {Grid, Grow, Typography} from '@mui/material'
-import useStyles from './Styles.js'
+
 
 
 
 const NewsCards = (props) => {
-    console.log(props)
+ 
     const news = props.articles
+    const newsLength = props.articles.length
+    console.log(newsLength)
     var count = 0
+
+    const infoCards = [
+        { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+        { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+        { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+        { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+      ];
+      
+      if(newsLength === 0){
+        return(
+            <Grid  container alignItems='stretch' spacing={3} style={{padding: '0 5%', width: '100%', margin: 0,}}>
+                  {
+                      infoCards.map((infoCard)=>(
+                             <Grid item xs ={12} sm ={6} md={4} lg={3} style={{display: 'flex', flexDirection: 'column', textAlign: 'center',}}>
+                                   <div style={{display: 'flex',flexDirection: 'column',justifyContent: 'space-between',alignItems: 'center',width: '100%',height: '45vh', padding: '10%',borderRadius: 10,color: 'white',backgroundColor:infoCard.color}} >
+                                          <Typography variant='h5' >{infoCard.title}</Typography>
+                                          {infoCard.info? (<Typography variant='h6' >
+                                              <strong>
+                                                  {infoCard.title}
+                                              </strong>
+                                              <br/>
+                                             {infoCard.info}
+                                           </Typography>) : null}
+                                          <Typography variant='h6' ><i>Try saying : <br/> {infoCard.text}</i></Typography>
+                                   </div>
+                             </Grid>
+
+                      ))
+                  }
+           </Grid>
+            
+            
+        )
+    }
+    
   
   return (
 <Grow in>
-      <Grid  container alignItems='stretch' spacing={3}>
+      <Grid  container alignItems='stretch' spacing={3} style={{padding: '0 5%', width: '100%', margin: 0,}}>
          {
              Object.keys(news).map( i =>(
                  <Grid key={i} item xs={12} sm ={6} md ={4} lg ={3} style={{display:'flex'}}>
@@ -25,6 +62,8 @@ const NewsCards = (props) => {
 </Grow>
  
   )
+
+ 
 }
 
 export default NewsCards
